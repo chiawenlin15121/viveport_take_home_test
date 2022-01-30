4270824752c632361ddfd3d080bed827db582797
Kevin Lin <chiawen.lin15121@gmail.com>
2022-01-30 20:41:31 +0800

    feat: init DataListPage and components

---
+++ src/components/DataList/Card.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .border.p-2.hover.d-flex.flex-column(@click="onClick('test')") 
+    .d-flex.justify-content-between.align-items-center
+      h2.mr-1.text-truncate.title {{title}}
+      h3 {{remark}}
+    .mt-2 {{description}}
+
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    remark: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    onClick:{
+      type: Function,
+      default: () => {}
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.title{
+  font-size: 1.2em;
+  font-weight: 300;
+}
+</style>
---
+++ src/pages/DataListPage.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .container-fluid
+    h1.text-center 台北市公園資料
+    DataList
+</template>
+
+<script>
+import DataList from "../components/DataList/DataList.vue";
+export default {
+  components: { DataList },
+};
+</script>
+
+<style>
+</style>
